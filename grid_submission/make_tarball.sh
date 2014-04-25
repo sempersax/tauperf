356d06017de095ba2100a5ba50101f7a67298e14
qbuat <quentin.buat@cern.ch>
2014-04-25 09:35:23 -0700

    continue cleaning

--- grid_submission/make_tarball.sh
+++ grid_submission/make_tarball.sh
@@ -2,14 +2,13 @@
 
 TAR_BALL=analysis.tar.gz
 SKIM_DIR=../skim
-SUBSTRUCTURE_DIR=../substructure_tools
 
 # PY_SCRIPT_1=../D3PD_slimmer
 PY_SCRIPT_1=../skimmer
 PY_SCRIPT_2=../split-skim
 
 echo "-- Tarball the analysis --"
-tar cvzf ${TAR_BALL} ${SKIM_DIR} ${SUBSTRUCTURE_DIR} ${PY_SCRIPT_1} ${PY_SCRIPT_2}
+tar cvzf ${TAR_BALL} ${SKIM_DIR} ${PY_SCRIPT_1} ${PY_SCRIPT_2}
 echo "-- Tarball rootpy --"
 tar czf rootpy.tar.gz ../rootpy
 echo "-- done ! --"
