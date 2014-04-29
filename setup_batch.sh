34604f10ebf15c6189fe3bfef25770522d26d051
qbuat <quentin.buat@cern.ch>
2014-04-29 11:18:57 -0700

    fixes in the trimming

---
+++ setup_batch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function sfu()
+{
+    # setup ROOT, Python etc.
+    #export ROOT_VERSION=5.34.09.hsg7 #5.34.07
+    export ROOT_VERSION=5.34.10 # skims
+    #export ROOT_VERSION=5.34.head
+    #export ROOT_VERSION=5.34.14
+    source /atlas/software/bleedingedge/setup.sh
+
+    # setup Python user base area
+    export PYTHONUSERBASE=${homenodepath}/local/${OS_VERSION}
+    export PATH=${PYTHONUSERBASE}/bin${PATH:+:$PATH}
+}
+
+sfu
