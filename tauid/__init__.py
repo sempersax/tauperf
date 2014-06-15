7ba71f356847d39766bba2e7c697c0ede6ff5bd5
qbuat <quentin.buat@cern.ch>
2014-06-15 11:29:44 -0700

    minor update

--- tauid/__init__.py
+++ tauid/__init__.py
@@ -2,6 +2,8 @@ import os
 import logging
 from variables import *
 
+import rootpy
+rootpy.log.basic_config_colorized()
 log = logging.getLogger('tauid')
 if not os.environ.get('DEBUG', False):
     log.setLevel(logging.INFO)
