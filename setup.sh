f826fa0632a4efbffed439e141dcb9b2cebae6d6
qbuat <quentin.buat@cern.ch>
2014-06-22 03:05:55 -0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 setupATLAS
-
-localSetupROOT 5.34.18-x86_64-slc5-gcc4.3
+localSetupROOT 5.34.10-x86_64-slc5-gcc4.3
 current_dir=${PWD}
 cd ../packages ;source RootCore/scripts/setup.sh; cd ${current_dir}
 
