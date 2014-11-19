8054809a07ad336ad7362b1cf69ff574d7a22335
qbuat <quentin.buat@cern.ch>
2014-11-19 12:44:00 +0100

    add bdt calculation

--- tauperf/extern/__init__.py
+++ tauperf/extern/__init__.py
@@ -1,18 +1,18 @@
 import rootpy.compiled as C
 import os
 HERE = os.path.dirname(os.path.abspath(__file__))
-C.register_file(os.path.join(HERE, '1p_odd_BDT.class.C'),
+C.register_file(os.path.join(HERE, '1p_odd_BDT_1p_odd.class.C'),
                 ['ReadBDT_1p_odd'])
-C.register_file(os.path.join(HERE, 'mp_odd_BDT.class.C'),
+C.register_file(os.path.join(HERE, 'mp_odd_BDT_mp_odd.class.C'),
                 ['ReadBDT_mp_odd'])
-C.register_file(os.path.join(HERE, '1p_even_BDT.class.C'),
+C.register_file(os.path.join(HERE, '1p_even_BDT_1p_even.class.C'),
                 ['ReadBDT_1p_even'])
-C.register_file(os.path.join(HERE, 'mp_even_BDT.class.C'),
+C.register_file(os.path.join(HERE, 'mp_even_BDT_mp_even.class.C'),
                 ['ReadBDT_mp_even'])
 
 from rootpy.compiled import (
-    ReadBDT_1p_odd, ReadBDT_1p_odd, 
-    ReadBDT_mp_even, ReadBDT_mp_even)
+    ReadBDT_1p_odd, ReadBDT_1p_even, 
+    ReadBDT_mp_odd, ReadBDT_mp_even)
 
 __all__ = [
     'ReadBDT_1p_odd',
