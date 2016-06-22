3abd61642cd000d5562bd5bc62034eb08d4c75a2
qbuat <quentin.buat@cern.ch>
2016-06-22 10:52:12 +0200

    add the no weight argument to plot-feature + path to Quentin mac

--- tauperf/__init__.py
+++ tauperf/__init__.py
@@ -5,15 +5,15 @@ import rootpy
 DEFAULT_STUDENT = 'Ztautau'
 DEFAULT_TREE = 'tau'
 
-if 'lxplus' in os.getenv('HOSTNAME'):
+if os.getenv('HOSTNAME') is not None and 'lxplus' in os.getenv('HOSTNAME'):
     UNMERGED_NTUPLE_PATH = '/afs/cern.ch/user/q/qbuat/CrackPotTauID/data/'
     NTUPLE_PATH = UNMERGED_NTUPLE_PATH
 else:
     UNMERGED_NTUPLE_PATH = os.path.join(
         # os.getenv('DATA_AREA'), 'crackpotauid_ntuples', 'v3')
-        os.getenv('DATA_AREA'), 'tauid_ntuples', 'v3')
+        os.getenv('DATA_AREA'), 'tauid_ntuples', 'v1')
     NTUPLE_PATH = UNMERGED_NTUPLE_PATH
-    NTUPLE_PATH = os.path.join(UNMERGED_NTUPLE_PATH, 'new_Z_training_sample')
+#     NTUPLE_PATH = os.path.join(UNMERGED_NTUPLE_PATH, 'new_Z_training_sample')
     # NTUPLE_PATH = os.path.join(UNMERGED_NTUPLE_PATH, 'nocorr_on_sumptfrac')
 
 
