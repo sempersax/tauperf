58fad908bb10b493a48f1ee252967fe4ce015267
qbuat <quentin.buat@cern.ch>
2016-09-25 16:54:35 +0200

    add stuff

---
+++ tauperf/branches.py
@@ -0,0 +1,28 @@
+__all__ = [
+    evt_branches,
+    kin_branches,
+    match_branches,
+    off_id_branches,
+    trig_id_branches,
+]
+
+
+evt_branches = [
+    'averageintpercrossing',
+    'good_npv'
+]
+
+kin_branches = [
+    'off_pt',
+    'off_eta',
+    'off_ntracks',
+    ]
+
+
+old_tauid_branches = [
+    'off_bdtjetscore',
+    'off_is_loose',
+    'off_is_medium',
+    'off_is_tight',
+    ]
+
