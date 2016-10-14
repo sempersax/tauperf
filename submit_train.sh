94cad03fc8fcddbb1a9d33ec348291322e9369a2
qbuat <quentin.buat@cern.ch>
2016-10-14 15:55:26 +0200

    more cleaning

--- submit_train.sh
+++
@@ -1,6 +0,0 @@
-
-
-nohup ./train --trigger --level hlt --features features --categories training_hlt & 
-nohup ./train --trigger --level hlt --features features_pileup_corrected --categories training_hlt &
-nohup ./train --features features --categories training & 
-nohup ./train --features features_pileup_corrected --categories training &
