bd2546dda8749830159d83f2fbce7082f70354eb
qbuat <quentin.buat@cern.ch>
2017-02-14 20:30:19 +0100

    change precision

--- tauperf/imaging/plotting.py
+++ tauperf/imaging/plotting.py
@@ -71,9 +71,9 @@ def plot_confusion_matrix(cm, classes,
 
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
+        plt.text(j, i, int(100 * cm[i, j]),
                  horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+                 color="white" if cm[i, j] > 100 * thresh else "black")
 
     plt.xlabel('True Tau Decay Mode')
     plt.ylabel('Reconstructed Tau Decay Mode')
