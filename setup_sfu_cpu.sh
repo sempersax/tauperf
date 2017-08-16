854f36e47dfa74a182e2026fe84be639ffe559aa
qbuat <quentin.buat@cern.ch>
2017-08-16 08:31:22 -0700

    add sfu cpu setup script

---
+++ setup_sfu_cpu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "specific to SFU GPU cluster"
+setupATLAS
+lsetup "root 6.04.10-x86_64-slc6-gcc48-opt"
+
+export VE_PATH=/home/qbuat/imaging/imaging_ve_cpu
+export DATA_AREA=/cluster/warehouse/qbuat/crackpot
+
+source ${VE_PATH}/bin/activate
+
+
+SOURCE_TAUPERF_SETUP="${BASH_SOURCE[0]:-$0}"
+DIR_TAUPERF_SETUP="$( dirname "$SOURCE_TAUPERF_SETUP" )"
+
+while [ -h "$SOURCE_TAUPERF_SETUP" ]
+do 
+  SOURCE_TAUPERF_SETUP="$(readlink "$SOURCE_TAUPERF_SETUP")"
+  [[ $SOURCE_TAUPERF_SETUP != /* ]] && SOURCE_TAUPERF_SETUP="$DIR_TAUPERF_SETUP/$SOURCE_TAUPERF_SETUP"
+  DIR_TAUPERF_SETUP="$( cd -P "$( dirname "$SOURCE_TAUPERF_SETUP"  )" && pwd )"
+  echo $SOURCE_TAUPERF_SETUP
+  echo $DIR_TAUPERF_SETUP
+done
+DIR_TAUPERF_SETUP="$( cd -P "$( dirname "$SOURCE_TAUPERF_SETUP" )" && pwd )"
+
+echo $DIR_TAUPERF_SETUP
+echo "sourcing ${SOURCE_TAUPERF_SETUP}..."
+
+export PATH=${DIR_TAUPERF_SETUP}${PATH:+:$PATH}
+export PYTHONPATH=${DIR_TAUPERF_SETUP}${PYTHONPATH:+:$PYTHONPATH}
+
+# specific to the gpu cluster
+#export THEANO_FLAGS='floatX=float32,device=gpu0,lib.cnmem=0.8'
+git pull origin imaging
